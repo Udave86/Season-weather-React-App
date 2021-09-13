44387a2a4a6a172f6305033c76c2e760b92e745c
Uday Dave <udave86@gmail.com>
2021-09-13 09:46:58 +0530

    Inital first commit

---
+++ src/index.js
@@ -0,0 +1,50 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+
+class App extends React.Component{
+constructor(props){
+  super(props);
+
+  this.state = { lat: null, errorMessage: ""};
+
+  //geolocation
+     window.navigator.geolocation.getCurrentPosition(
+  (position) => {
+    this.setState({
+      lat: position.coords.latitude
+    });
+  },
+  (err) => {
+    this.setState({
+      errorMessage: err.message
+    });
+  } 
+);
+}
+
+  //React sats we have to define render!!
+  render(){
+    if(this.state.errorMessage && !this.state.lat){
+      return (
+        <div> Error: {this.state.errorMessage}</div>
+      );
+    }
+    else if(!this.state.errorMessage && this.state.lat){
+      return (
+        <div>Latitude: {this.state.lat}</div>
+      );
+    }
+    else {
+      return(
+        <div>loading!</div>
+      );
+    }
+  }
+} 
+
+ReactDOM.render(<App />, document.querySelector("#root"));
