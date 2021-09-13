a435d5433c318b49d42f2304d0c138300d4fdeb9
Uday Dave <udave86@gmail.com>
2021-09-13 16:22:56 +0530

    season text changes done

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React from "react";
 
-const SeasonDisplay = () => {
-  return <div>Season Display</div>;
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "Summer" : "Winter";
+  } else {
+    return lat > 0 ? "Winter" : "Summer";
+  }
+};
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  //text display
+  const text =
+    season === "Winter" ? "Burr, It;s chilly" : "Let's hit the beach";
+  return <h1>{text}</h1>;
 };
 
 export default SeasonDisplay;
