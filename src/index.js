32f924467ccc0c153a2bdd36aa3f4bcd77add436
Uday Dave <udave86@gmail.com>
2021-09-13 16:23:13 +0530

    season text changes v2 done

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import SeasonDisplay from "./SeasonDisplay";
+
 if (module.hot) {
   module.hot.accept();
 }
 
 
 class App extends React.Component{
-constructor(props){
-  super(props);
 
-  this.state = { lat: null, errorMessage: ""};
+state = { lat: null, errorMessage: ""};
 
+componentDidMount(){
   //geolocation
      window.navigator.geolocation.getCurrentPosition(
   (position) => {
@@ -36,7 +37,7 @@ constructor(props){
     }
     else if(!this.state.errorMessage && this.state.lat){
       return (
-        <div>Latitude: {this.state.lat}</div>
+        < SeasonDisplay lat= {this.state.lat}/>
       );
     }
     else {
