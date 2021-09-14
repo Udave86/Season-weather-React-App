701b3b542eca45e4424c12600607e09e4d7cf014
Uday Dave <udave86@gmail.com>
2021-09-14 15:41:18 +0530

    version 4

--- src/index.js
+++ src/index.js
@@ -2,50 +2,41 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 if (module.hot) {
   module.hot.accept();
 }
 
+class App extends React.Component {
+  state = { lat: null, errorMessage: "" };
 
-class App extends React.Component{
-
-state = { lat: null, errorMessage: ""};
-
-componentDidMount(){
-  //geolocation
-     window.navigator.geolocation.getCurrentPosition(
-  (position) => {
-    this.setState({
-      lat: position.coords.latitude
-    });
-  },
-  (err) => {
-    this.setState({
-      errorMessage: err.message
-    });
-  } 
-);
-}
+  componentDidMount() {
+    //geolocation
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          lat: position.coords.latitude,
+        });
+      },
+      (err) => {
+        this.setState({
+          errorMessage: err.message,
+        });
+      }
+    );
+  }
 
   //React sats we have to define render!!
-  render(){
-    if(this.state.errorMessage && !this.state.lat){
-      return (
-        <div> Error: {this.state.errorMessage}</div>
-      );
-    }
-    else if(!this.state.errorMessage && this.state.lat){
-      return (
-        < SeasonDisplay lat= {this.state.lat}/>
-      );
-    }
-    else {
-      return(
-        <div>loading!</div>
-      );
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div> Error: {this.state.errorMessage}</div>;
+    } else if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    } else {
+      return <Spinner />;
     }
   }
-} 
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
